dc2773827a3ab5575ee4b64c06d649ef0acc727b
paulm130 <158609527+paulm130@users.noreply.github.com>
2026-04-21 12:57:40 -0700

    Added part2-question16.js

---
+++ expose/javascript/part2-question16.js
@@ -0,0 +1,12 @@
+let statistics = {
+    redCars: 21,
+    blueCars: 45,
+    greenCars: 12,
+    raceCars: 5,
+    blackCars: 40,
+    rareCars: 2
+};
+
+for(const property in statistics) {
+    console.log(`${property}: ${statistics[property]}`);
+}
